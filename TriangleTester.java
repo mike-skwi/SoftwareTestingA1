d22a5e8dd401dad950d83d274be9883a2a324f20
Skwierawski <mskwierawski@lbpc.com>
2019-01-16 17:07:28 -0600

    added Scanner in instead of cli

--- TriangleTester.java
+++ TriangleTester.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class TriangleTester{
 
-
+	
 	public TriangleTester(int side1, int side2, int side3){
 		// TODO make a case for negatives
 		// TODO Change it from accepting inputs from the cli to system in
@@ -26,36 +28,48 @@ public class TriangleTester{
 	}
 
 	public static void main(String[] args){
-
-
+		// Make sure this only takes positive integers.
+		// TODO make a new for loop. 
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter an integer: ");
+		int entered = 0;
 		int[] nums = new int[3];
+	
+		for(int i = 0; i<nums.length; i++){
+			System.out.println("\nEnter an integer: ");
+			nums[i] = reader.nextInt();		
+			//nums[entered] = n;
+			//entered++;
+		}
+		
+
 		//Check if there are exactly 3 command line args
-		if(args.length!=3 ){
-			System.out.println("Incorrect Input. You need to enter 3 inputs");
+		//if(args.length!=3 ){
+		//	System.out.println("Incorrect Input. You need to enter 3 inputs");
 			// Mark as failed //
 			//throw new IllegalArgumentException("Please enter 3 different integers");
-		}
+		//}
 		// Turn command line args into ints
-		else{
+		//else{
 			// Here's the main for loop.
 			// With this I should be able to:
 			// put everthing in the correct array/parse the strings to int
 			// test what kind of triangle it is
-			for( int i = 0; i < 3; i++ ){
+		//	for( int i = 0; i < 3; i++ ){
 
 				// Catch an error if someone puts in a string/anything else.
-				try{
-					nums[i] = Integer.parseInt(args[i]);
-				}
-				catch(Exception notInt){
-					System.out.println("Error: Non integer entered.");
-				}
+		//		try{
+		//			nums[i] = Integer.parseInt(args[i]);
+		//		}
+		//		catch(Exception notInt){
+		//			System.out.println("Error: Non integer entered.");
+		//		}
 
-			}
+		//	}
 
 		}
-		TriangleTester theTest = new TriangleTester(nums[0],nums[1],nums[2]);
+		//TriangleTester theTest = new TriangleTester(nums[0],nums[1],nums[2]);
 
 
 	}
-}
+
